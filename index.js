e3e8439eae7e995e05683073ad1b21d85e8e9a2f
bobnearents <bobnearents@yahoo.com>
2019-04-09 12:18:18 -0700

    finished iBst and findHeight

--- index.js
+++ index.js
@@ -1,125 +1,171 @@
-class BinarySearchTree{
-    constructor(key = null, value = null, parent = null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
+/* eslint-disable indent */
+'use strict';
+
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this)
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      return this.value;
+    }
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
         this.left = null;
         this.right = null;
+      }
     }
+  }
 
-    insert(key, value) {
-        if(this.key === null){
-            this.key = key;
-            this.value = value;
-        }
-        else if (key < this.key) {
-            if(this.left === null) {
-                this.left = new BinarySearchTree(key, value, this)
-            }
-            else {
-                this.left.insert(key, value);
-            }
-        }
-        else {
-            if (this.right === null) {
-                this.right = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.right.insert(key, value);
-            }
-        }
+  _findMin() {
+    if (!this.left) {
+      return this;
     }
-
-    find(key) {
-        if(this.key === key){
-            return this.value;
-        }
-        else if (key < this.key && this.left) {
-            return this.left.find(key);
-        }
-        else if (key > this.key && this.right) {
-            return this.right.find(key);
-        }
-        else {
-            throw new Error('Key Error');
-        }
-    }
-
-    remove(key) {
-        if(this.key === key) {
-            if(this.left && this.right) {
-                const successor = this.right._findMin();
-                this.key = successor.key;
-                this.value = successor.value;
-                successor.remove(successor.key);
-            }
-            else if (this.left) {
-                this._replaceWith(this.left);
-            }
-            else if (this.right) {
-                this._replaceWith(this.right);
-            }
-            else {
-                this._replaceWith(null);
-            }
-        }
-        else if (key < this.key && this.left) {
-            this.left.remove(key);
-        }
-        else if (key > this.key && this.right) {
-            this.right.remove(key);
-        }
-        else {
-            throw new Error('Key Error');
-        }
-    }
-
-    _replaceWith(node) {
-        if(this.parent) {
-            if(this === this.parent.left) {
-                this.parent.left = node;
-            }
-            else if(this === this.parent.right){
-                this.parent.right = node;
-            }
-            if(node) {
-                node.parent = this.parent;
-            }
-        }
-        else{
-            if(node){
-                this.key = node.key;
-                this.value = node.value;
-                this.left = node.left;
-                this.right = node.right;
-            }
-            else{
-                this.key = null;
-                this.value = null;
-                this.left = null;
-                this.right = null;
-            }
-        }
-    }
-
-    _findMin() {
-        if(!this.left) {
-            return this;
-        }
-        return this.left._findMin();
-    }
+    return this.left._findMin();
+  }
 }
 
-function findHeight(){
-    // Traverse to the lowest possible node
-    // Use that node as a key to traverse from the parent to that node and then count at each step
-    // to that node
-    
-
+function findHeight(tree) {
+  if (tree.left === null && tree.right === null) {
+    return 0;
+	}
+	else if (tree.left === null) {
+		return findHeight(tree.right) + 1;
+	}
+	else if (tree.right === null) {
+		return findHeight(tree.left) + 1;
+	}
+  else {
+    return Math.max(findHeight(tree.right), findHeight(tree.left)) + 1;
+	}
 }
 
-function main(){
+function isBst(tree) {
+	if (tree.left === null && tree.right === null) {
+		return true;
+	}
+	else if (tree.left === null) {
+		if (tree.right.key >= tree.key) {
+			return isBst(tree.right);
+		}
+		return false;
+	}
+	else if (tree.right === null) {
+		if (tree.left.key < tree.key) {
+			return isBst(tree.left);
+		}
+		return false;
+	}
+	else {
+		if ((tree.left.key < tree.key) && (tree.right.key >= tree.key)) {
+			return isBst(tree.left) && isBst(tree.right);
+		}
+		return false;
+	}
+}
 
+function main() {
+	let tree = new BinarySearchTree();
+	tree.insert(2, '');
+	tree.insert(6, '');
+	tree.insert(1, '');
+	tree.insert(9, '');
+	tree.insert(0, '');
+	tree.insert(5, '');
+	tree.insert(4, '');
+	tree.insert(1, '');
+	tree.insert(2, '');
+	tree.insert(7, '');
+	tree.insert(2, '');
+	console.log(isBst(tree));
 }
 
 main();
